28b852a49738deef4fc8aba1f1d6abacd2ad81ea
koba7taku <58512543+koba7taku@users.noreply.github.com>
2019-12-22 19:18:39 +0900

    automatic update create3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,8 +22,6 @@ $(function(){
       //メッセージが入ったHTMLに、入れ物ごと追加
       $('.messages').append(insertHTML)
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-        // $("#new_message")[0].reset();
-        // $(".submit-btn").prop("disabled", false);
       }
     })
     .fail(function() {
